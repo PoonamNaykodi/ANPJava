e1d47f294f2c8578a869e4eb3c5808df03e82f03
PoonamNaykodi <92991922+PoonamNaykodi@users.noreply.github.com>
2023-02-10 18:55:49 +0530

    Add files via upload

---
+++ ThursdayLab/firstspringboot/firstspringbootproject2/src/main/java/com/example/Firstspringbootproject2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Firstspringbootproject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Firstspringbootproject2Application.class, args);
+	}
+
+}
